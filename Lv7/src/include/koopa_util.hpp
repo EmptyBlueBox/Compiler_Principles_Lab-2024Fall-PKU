a064fc723ef3c37a12390619b5c816e644231899
Yutong Liang <lyt0112@outlook.com>
2024-12-26 15:40:43 +0800

    ✨ feat: Lv7 init

---
+++ Lv7/src/include/koopa_util.hpp
@@ -0,0 +1,132 @@
+/**
+ * @brief 定义一些通用的类, 比如符号表等
+ * @author Yutong Liang
+ * @date 2024-11-13
+ */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <utility>
+
+/**
+ * @brief 用于存储计算结果的类，可以是符号或立即数。
+ * @note 如果当前函数会产生一个计算结果, 那么这个计算结果会存储在返回的 `Result` 类型的变量中
+ * @note 比如 `PrimaryExpAST` 的 `print` 函数, 当它是从数字规约而来时, 它的 `Result` 变量会被初始化为立即数, 返回 `Result(Result::Type::IMM, *number)` 这样一个变量
+ * @note 如果当前函数不会产生计算结果, 那么返回的 `Result` 变量会被初始化为立即数 0
+ * @date 2024-11-27
+ */
+class Result
+{
+public:
+    /**
+     * @brief 当前计算值存储在 `%current_value_symbol_index` 这个寄存器中
+     * @date 2024-11-27
+     */
+    static int current_symbol_index;
+
+    enum class Type
+    {
+        IMM, // 立即数
+        REG  // 寄存器
+    };
+    Type type;                          // 结果的类型
+    int val;                            // 结果的值
+    bool control_flow_returned = false; // 控制流是否已经返回, 比如普通的 return 语句, 或者 if ... else ... 两个都 return 了就返回 true
+
+    // 默认构造函数，初始化为立即数 0, 没有用到的地方
+    Result() : type(Type::IMM), val(0) {}
+
+    // 带有指定类型的构造函数, 主要用来初始化寄存器
+    Result(Type type) : type(type), val(0)
+    {
+        if (type == Type::REG)
+        {
+            val = ++current_symbol_index;
+        }
+    }
+
+    // 带有指定类型和值的构造函数, 主要用来初始化立即数
+    Result(Type type, int val) : type(type), val(val)
+    {
+        if (type == Type::REG)
+        {
+            val = ++current_symbol_index;
+        }
+    }
+
+    // 重载 <<
+    friend std::ostream &operator<<(std::ostream &os, const Result &result)
+    {
+        os << (result.type == Result::Type::REG ? "%" : "") << result.val;
+        return os;
+    }
+};
+
+/**
+ * @brief 符号
+ * @date 2024-12-22
+ */
+class Symbol
+{
+public:
+    enum class Type
+    {
+        VAR,
+        VAL
+    };
+    Type type;
+    int val; // 如果 type 是 VAL, 那么 val 是立即数的数值; 如果 type 是 VAR, 那么 val 是变量的层级, 比如 `a = 2;` 如果在符号表中在层级 1 找到这个符号, 那么就会返回 1, 得到 @a_1
+    Symbol() : type(Type::VAL), val(0) {}
+    Symbol(Type type, int val) : type(type), val(val) {}
+};
+
+/**
+ * @brief 上下文管理器, 用于管理符号表和 If ... Else ... 语句的数量, 从而输出恰当的 %then_1: 和 %else_1:
+ * @date 2024-12-22
+ */
+class KoopaContextManager
+{
+private:
+    // 每进入一个块, 就创建一个新的符号表, 块包括函数的大括号和语句块的大括号
+    std::vector<std::unordered_map<std::string, Symbol>> symbol_tables;
+    // 用于判断当前符号是否在当前下标被分配, 比如 @a_1 在 symbol_tables[0] 中被分配, 那么 is_symbol_allocated_in_this_level[std::make_pair("a", 1)] == true
+    // 用来避免如下的例子中 a 被分配了两次
+    // int a = 1;
+    // if (a) {
+    //     {int a = 2;}
+    //     {int a = 3;}
+    // }
+    // 这样的重复分配 @a_2 是错误的
+    // @a_2 = alloc i32
+    // store 2, @a_2
+    // @a_2 = alloc i32
+    // store 3, @a_2
+    std::map<std::pair<std::string, int>, bool> _is_symbol_allocated_in_this_level;
+
+public:
+    // 当前的 if ... else ... 语句数量, 遇见一个加一, 离开一个 **不** 减一
+    int total_if_else_statement_count = 0;
+
+    // 每进入一个大括号 (或者 if ... else ... 语句) 就创建一个新的符号表
+    void new_symbol_table_hierarchy();
+
+    // 离开大括号 (或者 if ... else ... 语句) 就删除一个符号表
+    void delete_symbol_table_hierarchy();
+
+    // 符号表操作, 在当前符号表中插入一个符号对应的字符串 (不包含后缀) 和 Symbol 对象 (立即数还是变量的层级)
+    void insert_symbol(const std::string &name, Symbol symbol);
+
+    // 在当前符号表中查找一个符号对应的 Symbol 对象, 从最内层的符号表开始查找, 逐渐向外层查找
+    Symbol name_to_symbol(const std::string &name);
+
+    // 设置一个符号在当前层级的符号表中被分配
+    void set_symbol_allocated_in_this_level(const std::string &name);
+
+    // 判断一个符号在当前层级的符号表中是否被分配
+    bool is_symbol_allocated_in_this_level(const std::string &name);
+};
---
+++ Lv7/src/riscv_util.cpp
@@ -0,0 +1,285 @@
+#include <iostream>
+
+#include "include/riscv_util.hpp"
+
+void StackManager::save_value_to_stack(const koopa_raw_value_t &value)
+{
+    // 如果 value 不在 value_to_stack_offset 中, 则需要分配新的空间
+    if (value_to_stack_offset.find(value) == value_to_stack_offset.end())
+    {
+        value_to_stack_offset[value] = stack_used_byte;
+        stack_used_byte += 4;
+        if (stack_used_byte > stack_size)
+        {
+            throw std::runtime_error("save_value_to_stack: stack overflow");
+        }
+    }
+    else
+    {
+        // 如果 value 在 value_to_stack_offset 中, 则不用分配新的空间, 什么都不用干
+    }
+}
+
+int StackManager::get_stack_used_byte() const
+{
+    return stack_used_byte;
+}
+
+int StackManager::get_num_stack_frame_byte() const
+{
+    return stack_size;
+}
+
+int StackManager::get_value_stack_offset(const koopa_raw_value_t &value)
+{
+    if (value_to_stack_offset.find(value) == value_to_stack_offset.end())
+    {
+        throw std::runtime_error("get_value_stack_offset: value not found in this stack frame");
+    }
+    return value_to_stack_offset[value];
+}
+
+void RISCVContextManager::set_reg_free(const koopa_raw_value_t &value)
+{
+    if (_value_to_reg_string.find(value) == _value_to_reg_string.end())
+    {
+        throw std::runtime_error("set_reg_free: value not found in this stack frame");
+    }
+    _reg_is_used[value_to_reg_string(value)] = false;
+    _value_to_reg_string.erase(value);
+}
+
+bool RISCVContextManager::exist(const koopa_raw_value_t &value)
+{
+    return _value_to_reg_string.find(value) != _value_to_reg_string.end();
+}
+
+void RISCVContextManager::allocate_reg(const koopa_raw_value_t &value, bool is_zero)
+{
+    if ((_value_to_reg_string.find(value) != _value_to_reg_string.end()) && _reg_is_used[value_to_reg_string(value)] == true)
+    {
+        throw std::runtime_error("allocate_reg: value already allocated, the value kind is " + std::to_string(value->kind.tag) + "\n0: Integer, 8: Load, 9: Store, 12: Binary, 13: Branch, 14: Jump, 15: Call, 16: Return");
+    }
+    if (is_zero)
+    {
+        _set_value_to_reg_string(value, "x0");
+        return;
+    }
+    else
+    {
+        // 选择一个未被占用的寄存器
+        for (int i = 0; i <= 6; ++i)
+        {
+            if (!_reg_is_used["t" + std::to_string(i)])
+            {
+                _set_value_to_reg_string(value, "t" + std::to_string(i));
+                _reg_is_used["t" + std::to_string(i)] = true;
+                return;
+            }
+        }
+        for (int i = 0; i <= 7; ++i)
+        {
+            if (!_reg_is_used["a" + std::to_string(i)])
+            {
+                _set_value_to_reg_string(value, "a" + std::to_string(i));
+                _reg_is_used["a" + std::to_string(i)] = true;
+                return;
+            }
+        }
+    }
+}
+
+std::string RISCVContextManager::new_temp_reg()
+{
+    // 选择一个未被占用的寄存器
+    for (int i = 0; i <= 6; ++i)
+    {
+        if (!_reg_is_used["t" + std::to_string(i)])
+        {
+            std::string reg_name = "t" + std::to_string(i);
+            return reg_name;
+        }
+    }
+    for (int i = 0; i <= 7; ++i)
+    {
+        if (!_reg_is_used["a" + std::to_string(i)])
+        {
+            std::string reg_name = "a" + std::to_string(i);
+            return reg_name;
+        }
+    }
+    throw std::runtime_error("new_temp_reg: no free register found");
+}
+
+std::string RISCVContextManager::value_to_reg_string(const koopa_raw_value_t &value)
+{
+    if (_value_to_reg_string.find(value) == _value_to_reg_string.end())
+    {
+        throw std::runtime_error("value_to_reg_string: value not found");
+    }
+    return _value_to_reg_string[value];
+}
+
+void RISCVContextManager::_set_value_to_reg_string(const koopa_raw_value_t &value, const std::string &reg_string)
+{
+    if (_value_to_reg_string.find(value) != _value_to_reg_string.end())
+    {
+        throw std::runtime_error("_set_value_to_reg_string: value already exists in the map, the value kind is " + std::to_string(value->kind.tag) + "\n0: Integer, 12: Binary");
+    }
+    _value_to_reg_string[value] = reg_string;
+}
+
+StackManager &RISCVContextManager::get_current_function_stack_manager()
+{
+    if (_function_name_to_stack_manager.find(current_function_name) == _function_name_to_stack_manager.end())
+    {
+        throw std::runtime_error("get_current_function_stack_manager: function not found");
+    }
+    return _function_name_to_stack_manager[current_function_name];
+}
+
+void RISCVContextManager::init_stack_manager_for_one_function(const std::string &function_name, int stack_size)
+{
+    if (_function_name_to_stack_manager.find(function_name) != _function_name_to_stack_manager.end())
+    {
+        throw std::runtime_error("init_stack_manager_for_one_function: function already exists");
+    }
+    _function_name_to_stack_manager[function_name] = StackManager(stack_size);
+    current_function_name = function_name;
+}
+
+void RISCVPrinter::ret()
+{
+    std::cout << "\tret" << std::endl;
+}
+
+void RISCVPrinter::seqz(const std::string &rd, const std::string &rs1)
+{
+    std::cout << "\tseqz " << rd << ", " << rs1 << std::endl;
+}
+
+void RISCVPrinter::snez(const std::string &rd, const std::string &rs1)
+{
+    std::cout << "\tsnez " << rd << ", " << rs1 << std::endl;
+}
+
+void RISCVPrinter::or_(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\tor " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::and_(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\tand " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::xor_(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\txor " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::add(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\tadd " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::addi(const std::string &rd, const std::string &rs1, const int &imm, RISCVContextManager &context_manager)
+{
+    if (imm >= -2048 && imm < 2048)
+    {
+        std::cout << "\taddi " << rd << ", " << rs1 << ", " << imm << std::endl;
+    }
+    else
+    {
+        std::string reg = context_manager.new_temp_reg();
+        li(reg, imm);
+        std::cout << "\tadd " << rd << ", " << rs1 << ", " << reg << std::endl;
+    }
+}
+
+void RISCVPrinter::sub(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\tsub " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::mul(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\tmul " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::div(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\tdiv " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::rem(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\trem " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::sgt(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\tsgt " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::slt(const std::string &rd, const std::string &rs1, const std::string &rs2)
+{
+    std::cout << "\tslt " << rd << ", " << rs1 << ", " << rs2 << std::endl;
+}
+
+void RISCVPrinter::li(const std::string &rd, const int &imm)
+{
+    std::cout << "\tli " << rd << ", " << imm << std::endl;
+}
+
+void RISCVPrinter::mv(const std::string &rd, const std::string &rs1)
+{
+    std::cout << "\tmv " << rd << ", " << rs1 << std::endl;
+}
+
+void RISCVPrinter::lw(const std::string &rd, const std::string &base, const int &bias, RISCVContextManager &context_manager)
+{
+    // 检查偏移量是否在 12 位立即数范围内
+    if (bias >= -2048 && bias < 2048)
+    {
+        std::cout << "\tlw " << rd << ", " << bias << "(" << base << ")" << std::endl;
+    }
+    else
+    {
+        std::string reg = context_manager.new_temp_reg();
+        li(reg, bias);
+        add(reg, reg, base);
+        std::cout << "\tlw " << rd << ", " << "(" << reg << ")" << std::endl;
+    }
+}
+
+void RISCVPrinter::sw(const std::string &rs1, const std::string &base, const int &bias, RISCVContextManager &context_manager)
+{
+    // 检查偏移量是否在 12 位立即数范围内
+    if (bias >= -2048 && bias < 2048)
+    {
+        std::cout << "\tsw " << rs1 << ", " << bias << "(" << base << ")" << std::endl;
+    }
+    else
+    {
+        std::string reg = context_manager.new_temp_reg();
+        li(reg, bias);
+        add(reg, reg, base);
+        std::cout << "\tsw " << rs1 << ", " << "(" << reg << ")" << std::endl;
+    }
+}
+
+void RISCVPrinter::bnez(const std::string &cond, const std::string &label)
+{
+    std::cout << "\tbnez " << cond << ", " << label << std::endl;
+}
+
+void RISCVPrinter::beqz(const std::string &cond, const std::string &label)
+{
+    std::cout << "\tbeqz " << cond << ", " << label << std::endl;
+}
+
+void RISCVPrinter::jump(const std::string &label)
+{
+    std::cout << "\tj " << label << std::endl;
+}
